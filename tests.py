8498cc6408c28c93e8a67185c4b6a606407828fa
Swaraj Kaondal <41962254+SwarajKaondal@users.noreply.github.com>
2024-08-26 16:17:04 -0400

    failt test

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ def test_pass():
     assert multiply([1,2,3]) == 6
     
 def test_fail():
-    assert multiply([1,2,3,4]) == 24
+    assert multiply([1,2,3,4]) == 23
 
 if __name__ == '__main__':
     test_pass()

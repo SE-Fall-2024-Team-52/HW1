fd8dc650426c8132d9c35a472a306012726d9198
Swaraj Kaondal <41962254+SwarajKaondal@users.noreply.github.com>
2024-08-29 21:35:29 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from src.myfile import multiply
 def test_pass():
     assert multiply([1,2,3]) == 6
     
-# def test_fail():
-#     assert multiply([1,2,3,4]) == 23
+def test_fail():
+    assert multiply([1,2,3,4]) == 23
 
 if __name__ == '__main__':
     test_pass()
